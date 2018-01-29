e953ca29bb09e5afcb264f5655fd49ab9b18bee8
fivepotatoes <35906518+fivepotatoes@users.noreply.github.com>
2018-01-28 21:12:18 -1000

    Update Femaleprograminitialize_page.php

--- Femaleprograminitialize_page.php
+++ Femaleprograminitialize_page.php
@@ -4,66 +4,54 @@
 
 		<h1>Actions</h1>
 		
-		<div align=center></center>
-			<a href="file://E|/programpage1.php">
-			<img width="350" height="275" align="bottom" src = "file://E|/ProgramImageDirectory/Actions/Walking.jpg">
-			</a>
+			<div align=center></center>
+				<a href="file://E|/programpage1.php">
+				<img width="350" height="275" align="bottom" src = "file://E|/ProgramImageDirectory/Actions/Walking.jpg">
+				</a>
 			
 		<h2>Body Parts</h2>
 		
-		<div align=center></center>
-			<a href="file://E|/programpage2.php">
-			<img width="300" height="275" align="bottom" src = "file://E|/ProgramImageDirectory/download.jpg">
-			</a>
+			<div align=center></center>
+				<a href="file://E|/programpage2.php">
+				<img width="300" height="275" align="bottom" src = "file://E|/ProgramImageDirectory/download.jpg">
+				</a>
 			
 		<h3>Food and Drinks</h3>
 		
-		<div align=center></center>
-			<a href="file://E|/programpage3.php">
-			<img width="500" height="275" align="bottom" src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Food.jpg">
-			</a>
+			<div align=center></center>
+				<a href="file://E|/programpage3.php">
+				<img width="500" height="275" align="bottom" src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Food.jpg">
+				</a>
 			
 			
-</body>
+	</body>
 
-<style type="text/css">
-    body {
-        margin-left:100px;
-        margin-right:100px;
-		margin-top:10px
-        line-height: 1.5;
-        FONT-FAMILY: Arial, Helvetica, sans-serif; 
-        font-size: 1.3em;
-	}
+	<style type="text/css">
+    		body {
+        		margin-left:100px;
+        		margin-right:100px;
+			margin-top:10px
+        		line-height: 1.5;
+        		FONT-FAMILY: Arial, Helvetica, sans-serif; 
+        		font-size: 1.3em;
+			}
 
-    h1 {
-		text-align:center;
-        font-weight: normal;
-        font-size: 20px;
-    }
+    		h1 {
+			text-align:center;
+        		font-weight: normal;
+        		font-size: 20px;
+    			}
 
-    h2 {
-		text-align:center;
-        font-weight: normal;
-        font-size: 20px;
-    }
+    		h2 {
+			text-align:center;
+        		font-weight: normal;
+        		font-size: 20px;
+    			}
 
-    h3 {
-        font-weight: bold;
-        font-size: 20px;
-    }
+   		h3 {
+        		font-weight: bold;
+        		font-size: 20px;
+   			}
 
-	<div onkeydown="myFunction()">
-
-<script>
-function myFunction() {
-    var x = document.createElement("VAR");
-    var t = document.createTextNode("Legs");
-    x.appendChild(t);
-    document.body.appendChild(x);
-}
-}
-
-</script>
 	 
 </html>
