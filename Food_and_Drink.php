931571b188dc420ea10322d3c63f014c77293f2c
fivepotatoes <35906518+fivepotatoes@users.noreply.github.com>
2018-01-28 21:30:27 -1000

    Update Food_and_Drink.php

--- Food_and_Drink.php
+++ Food_and_Drink.php
@@ -30,14 +30,6 @@
 			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Cheese.png" onclick = "myFunctionS()" alt="Submit" style="width: 100%">Cheese</p>
 			<p style="float: left; font-size: 12pt; text-align: center; width: 12%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/ChickenWings.jpg" onclick = "myFunctionT()" alt="Submit" style="width: 100%">Chicken wings</p>
 			<p style="float: left; font-size: 12pt; text-align: center; width: 12%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Cake.jpg" onclick = "myFunctionU()" alt="Submit" style="width: 100%">Cake</p> <p style="clear: both;">
-			
-<!			<img src="imageLocation" style="float: left; width: 30%; margin-right: 1%; margin-bottom: 0.5em;">
-
- 
-   <img src="imageLocation" style="float: left; width: 30%; margin-right: 1%; margin-bottom: 0.5em;">
-
-   
-<!  <img src="imageLocation" style="float: left; width: 30%; margin-right: 1%; margin-bottom: 0.1em;">
   
 		<br>
 		<br>
@@ -45,188 +37,161 @@
 			<a href="file://E|/Image_Translator_Program.html"><p style="float: center; font-size: 10pt; text-align: center; width: 10%; margin-right: 9%; margin-left: 9%; margin-bottom: 0.5em;"><img src = "file://E|/ProgramImageDirectory/Buttons/Back.png" img width="48" height="36"><p style="clear: both;">
 			</a>
 			
-	<div align=center></center>
-	<a href="file://E|/Image_Translator_Program.html">
-	<input type="submit" value="Back to Start">
-	</a>
-	<p style="clear: both;">
+		<div align=center></center>
+			<a href="file://E|/Image_Translator_Program.html">
+			<input type="submit" value="Back to Start">
+			</a>
+			<p style="clear: both;">
 	
-	<div align=center></center>
-	<a href="file://E|/Gender_page.php">
-	<input type="submit" value="Gender Selection">
-	</a>
-	<p style="clear: both;">
+		<div align=center></center>
+			<a href="file://E|/Gender_page.php">
+			<input type="submit" value="Gender Selection">
+			</a>
+			<p style="clear: both;">
 
-	<div align=center></center>
-	<a href="file://E|/Actions.php">
-	<input type="submit" value="Actions">
-	</a>
-	<p style="clear: both;">
+		<div align=center></center>
+			<a href="file://E|/Actions.php">
+			<input type="submit" value="Actions">
+			</a>
+			<p style="clear: both;">
 	
-	<div align=center></center>
-	<a href="file://E|/Body_Parts.php">
-	<input type="submit" value="Body Parts">
-	</a>
-	<p style="clear: both;">
+		<div align=center></center>
+			<a href="file://E|/Body_Parts.php">
+			<input type="submit" value="Body Parts">
+			</a>
+			<p style="clear: both;">
 	
-	<div align=center></center>
-	<a href="file://E|/Places.php">
-	<input type="submit" value="Places">
-	</a>
-	<p style="clear: both;">
+		<div align=center></center>
+			<a href="file://E|/Places.php">
+			<input type="submit" value="Places">
+			</a>
+			<p style="clear: both;">
 	
-	<div align=center></center>
-	<a href="file://E|/Emergency_and_Health_page.php">
-	<input type="submit" value="Emergency/Health">
-	</a>
-	<p style="clear: both;">
+		<div align=center></center>
+			<a href="file://E|/Emergency_and_Health_page.php">
+			<input type="submit" value="Emergency/Health">
+			</a>
+			<p style="clear: both;">
 	
 	
-	<div align=center></center>
-	<a href="file://E|/Feelings.php">
-	<input type="submit" value="Feelings">
-	</a>
-	<p style="clear: both;">
+		<div align=center></center>
+			<a href="file://E|/Feelings.php">
+			<input type="submit" value="Feelings">
+			</a>
+			<p style="clear: both;">
 
 
   
-<script>
-/* These are the functions that get called when the user clicks the buttons on the program page*/
-function myFunctionA() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Dairy");
-}
-function myFunctionB() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Grains");
-}
-function myFunctionC() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300"); 
-    myWindow.document.write("<p>Fruits");
-}
-function myFunctionD() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Vegetables");
-}
-function myFunctionE() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Meat");
-}
-function myFunctionF() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Milk");
-}
-function myFunctionG() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Apple");
-}
-function myFunctionH() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Ahi");
-}
-function myFunctionI() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Blueberries");
-}
-function myFunctionJ() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Burger");
-}
-function myFunctionK() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300"); 
-    myWindow.document.write("<p>Chocolate");
-}
-function myFunctionL() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Corn");
-}
-function myFunctionM() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Eggs");
-}
-function myFunctionN() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Broccoli");
-}
-function myFunctionO() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Water");
-}
-function myFunctionP() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Bread");
-}
-function myFunctionQ() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Cabbage");
-}
-function myFunctionR() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Carrots");
-}
-function myFunctionS() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Cheese");
-}
-function myFunctionT() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Chicken wings");
-}
-function myFunctionU() {
-    var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
-    myWindow.document.write("<p>Cake");
-}
-</script>
+	<script>
+	/* These are the functions that get called when the user clicks the buttons on the program page*/
+		function myFunctionA() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Dairy");
+		}
+		function myFunctionB() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Grains");
+		}
+		function myFunctionC() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300"); 
+   		 myWindow.document.write("<p>Fruits");
+		}
+		function myFunctionD() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Vegetables");
+		}
+		function myFunctionE() {
+   		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Meat");
+		}
+		function myFunctionF() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Milk");
+		}
+		function myFunctionG() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Apple");
+		}
+		function myFunctionH() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Ahi");
+		}
+		function myFunctionI() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+   		 myWindow.document.write("<p>Blueberries");
+		}
+		function myFunctionJ() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Burger");
+		}
+		function myFunctionK() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300"); 
+    		myWindow.document.write("<p>Chocolate");
+		}
+		function myFunctionL() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Corn");
+		}
+		function myFunctionM() {
+   		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Eggs");
+		}
+		function myFunctionN() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Broccoli");
+		}
+		function myFunctionO() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+   		myWindow.document.write("<p>Water");
+		}
+		function myFunctionP() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Bread");
+		}
+		function myFunctionQ() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Cabbage");
+		}
+		function myFunctionR() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Carrots");
+		}
+		function myFunctionS() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Cheese");
+		}
+		function myFunctionT() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+   		myWindow.document.write("<p>Chicken wings");
+		}
+		function myFunctionU() {
+    		var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
+    		myWindow.document.write("<p>Cake");
+		}
+	</script>
   
    
-<style type="text/css">
-    body {
-        margin-left:100px;
-        margin-right:100px;
-  margin-top:10px
-        line-height: 1.5;
-        FONT-FAMILY: Arial, Helvetica, sans-serif; 
-        font-size: 1.2em;
- }
-    h1 {
-        font-weight: bold;
-        font-size: 50px;
-    }
-    h2 {
-        font-weight: bold;
-        font-size: 24px;
-    }
-    h3 {
-        font-weight: bold;
-        font-size: 20px;
-    }
+	<style type="text/css">
+   		body {
+        		margin-left:100px;
+        		margin-right:100px;
+  			margin-top:10px
+        		line-height: 1.5;
+        		FONT-FAMILY: Arial, Helvetica, sans-serif; 
+        		font-size: 1.2em;
+ 			}
+    		h1 {
+        		font-weight: bold;
+        		font-size: 50px;
+    			}
+    		h2 {
+        		font-weight: bold;
+        		font-size: 24px;
+    			}
+    		h3 {
+        		font-weight: bold;
+        		font-size: 20px;
+    			}
 
-</body>
-</html>
-<style type="text/css">
-    body {
-        margin-left:100px;
-        margin-right:100px;
-		margin-top:10px
-        line-height: 1.5;
-        FONT-FAMILY: Arial, Helvetica, sans-serif; 
-        font-size: 1.2em;
-	}
-
-    h1 {
-        font-weight: bold;
-        font-size: 50px;
-    }
-
-    h2 {
-        font-weight: bold;
-        font-size: 24px;
-    }
-
-    h3 {
-        font-weight: bold;
-        font-size: 20px;
-    }
-
-</body>
+	</body>
 </html>
