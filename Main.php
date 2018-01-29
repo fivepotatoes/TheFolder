5129fe5c95715ef2e3f1c623bf0b2181a6f05066
fivepotatoes <35906518+fivepotatoes@users.noreply.github.com>
2018-01-28 21:40:51 -1000

    Update Main.php

--- Main.php
+++ Main.php
@@ -7,23 +7,17 @@
 			<img width="350" height="275" align="bottom" src = "file://E|/ProgramImageDirectory/Buttons/Reload.png">
 			</a>
 			
-</body>
+	</body>
 			
-<!DOCTYPE html>
-<html>
-<body>
+		<p>Click the button to open a new window called "MsgWindow" with some text.</p>
+		<button onclick="myFunction()">Try it</button>
 
-<p>Click the button to open a new window called "MsgWindow" with some text.</p>
+		<script>
+			function myFunction() {
+    			var myWindow = window.open("", "MsgWindow", "width=500,height=500");
+    			myWindow.document.write("<p>This is 'Hi'. I am 200px wide and 100px tall!</p>");
+			}
+		</script>
 
-
-<button onclick="myFunction()">Try it</button>
-
-<script>
-function myFunction() {
-    var myWindow = window.open("", "MsgWindow", "width=500,height=500");
-    myWindow.document.write("<p>This is 'Hi'. I am 200px wide and 100px tall!</p>");
-}
-</script>
-
-</body>
+	</body>
 </html>
