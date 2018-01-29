d36831c8ccb9687faa116e7e4a1d2b982248c397
fivepotatoes <35906518+fivepotatoes@users.noreply.github.com>
2018-01-28 20:55:02 -1000

    Update Category_page.php

--- Category_page.php
+++ Category_page.php
@@ -2,39 +2,27 @@
 <html>
 	<body>
 
-	<Row 1!>
+		<Row 1!>
 			<a href="file://E|/Actions.php"><p style="float: left; font-size: 20pt; text-align: center; width: 31%; margin-right: 1%; margin-top: 0.5em;">Actions<img src = "file://E|/ProgramImageDirectory/Actions/Walking.jpg" style="width: 100%"></p>
 			<a href="file://E|/Body_Parts.php"><p style="float: left; font-size: 20pt; text-align: center; width: 31%; margin-right: 1%; margin-top: 0.5em;">Body Parts<img src = "file://E|/ProgramImageDirectory/download.jpg" style="width: 100%"></p>
 			<a href="file://E|/Food_and_Drink.php"><p style="float: left; font-size: 20pt; text-align: center; width: 31%; margin-right: 1%; margin-top: 0.5em;">Food and Drinks<img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Food.jpg" style="width: 100%"></p> <p style="clear: both;">
 			
-	<Row 2!>	
+		<Row 2!>	
 			<a href="file://E|/Places.php"><p style="float: left; font-size: 20pt; text-align: center; width: 31%; margin-right: 1%; margin-top: 0.5em;">Places<img src = "file://E|/ProgramImageDirectory/Places/Places.jpg" style="width: 100%"></p>
 			<a href="file://E|/Emergency_and_Health_page.php"><p style="float: left; font-size: 20pt; text-align: center; width: 31%; margin-right: 1%; margin-top: 0.5em;">Emergency/Health<img src = "file://E|/ProgramImageDirectory/EmergencyHealth/redcross.png" style="width: 100%"></p>
 			<a href="file://E|/Feelings.php"><p style="float: left; font-size: 20pt; text-align: center; width: 31%; margin-right: 1%; margin-top: 0.5em;">Feelings<img src = "file://E|/ProgramImageDirectory/Feelings/Feelings.png" style="width: 100%"></p> <p style="clear: both;">
 			
-</body>
+	</body>
 
-<style type="text/css">
-    body {
-        margin-left:100px;
-        margin-right: 1%;
-		margin-top:0.5em
-        line-height: 1.5;
-        FONT-FAMILY: Arial, Helvetica, sans-serif; 
-        font-size: 1.3em;
-	}
+	<style type="text/css">
+    		body {
+        		margin-left:100px;
+        		margin-right: 1%;
+			margin-top:0.5em
+        		line-height: 1.5;
+        		FONT-FAMILY: Arial, Helvetica, sans-serif; 
+        		font-size: 1.3em;
+			}
 
-	<div onkeydown="myFunction()">
-
-<script>
-function myFunction() {
-    var x = document.createElement("VAR");
-    var t = document.createTextNode("Legs");
-    x.appendChild(t);
-    document.body.appendChild(x);
-}
-}
-
-</script>
 	 
 </html>
