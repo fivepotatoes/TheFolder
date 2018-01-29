750d0ae70c454bbd154a5e5b509b7c5feef589f7
fivepotatoes <35906518+fivepotatoes@users.noreply.github.com>
2018-01-28 21:35:47 -1000

    Update Gender_page.php

--- Gender_page.php
+++ Gender_page.php
@@ -12,29 +12,16 @@
 			<a href="file://E|/Category_page.php"><p style="float: left; font-size: 24pt; text-align: center; width: 30%; margin-right: 9%; margin-left: 9%; margin-bottom: 0.5em;"><img src = "file://E|/ProgramImageDirectory/SelectGenderPage/FemaleImage.jpg" style="width: 100%">Female</p>
 			</a>
 			
-</body>
+	</body>
 
-<style type="text/css">
-    body {
-        margin-left:100px;
-        margin-right:100px;
-		margin-top:10px
-        line-height: 1.5;
-        FONT-FAMILY: Arial, Helvetica, sans-serif; 
-        font-size: 1.3em;
-	}
-
-	<div onkeydown="myFunction()">
-
-<script>
-function myFunction() {
-    var x = document.createElement("VAR");
-    var t = document.createTextNode("Legs");
-    x.appendChild(t);
-    document.body.appendChild(x);
-}
-}
-
-</script>
-	 
+	<style type="text/css">
+    		body {
+        		margin-left:100px;
+       			margin-right:100px;
+			margin-top:10px
+        		line-height: 1.5;
+        		FONT-FAMILY: Arial, Helvetica, sans-serif; 
+        		font-size: 1.3em;
+			}
+ 
 </html>
