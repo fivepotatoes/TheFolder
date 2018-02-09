a73fb77045d86d67cdb6e717355d2b317c8520d8
fivepotatoes <35906518+fivepotatoes@users.noreply.github.com>
2018-02-09 01:04:50 -1000

    Update Food_and_Drink.php

--- Food_and_Drink.php
+++ Food_and_Drink.php
@@ -5,31 +5,31 @@
 		<div align=center></center>
 		
 		<!Row 1>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Dairy.jpg" onclick = "myFunctionA()" alt="Submit" style="width: 100%">Dairy</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Grains.jpg" onclick = "myFunctionB()" alt="Submit" style="width: 100%">Grains</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Fruit.jpg" onclick = "myFunctionC()" alt="Submit" style="width: 100%">Fruit</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Vegetables.jpg" onclick = "myFunctionD()" alt="Submit" style="width: 100%">Vegetables</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Meat.jpg" onclick = "myFunctionE()" alt="Submit" style="width: 100%">Meat</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Milk.jpg" onclick = "myFunctionF()" alt="Submit" style="width: 100%">Milk</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Apple.jpg" onclick = "myFunctionG()" alt="Submit" style="width: 100%">Apple</p> <p style="clear: both;">
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Dairy.jpg" onclick = "tdy()" alt="Submit" style="width: 100%">Dairy</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Grains.jpg" onclick = "tgs()" alt="Submit" style="width: 100%">Grains</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Fruit.jpg" onclick = "tft()" alt="Submit" style="width: 100%">Fruit</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Vegetables.jpg" onclick = "tvs()" alt="Submit" style="width: 100%">Vegetables</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Meat.jpg" onclick = "tmt()" alt="Submit" style="width: 100%">Meat</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Milk.jpg" onclick = "tmk()" alt="Submit" style="width: 100%">Milk</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Apple.jpg" onclick = "tae()" alt="Submit" style="width: 100%">Apple</p> <p style="clear: both;">
 			
 		<!Row 2>	
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Ahi.jpg" onclick = "myFunctionH()" alt "Submit" style="width: 100%">Ahi</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Blueberries.jpg" onclick = "myFunctionI()" alt "Submit" style ="width: 100%">Blueberries</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Burger.jpg" onclick = "myFunctionJ()" alt "Submit" style ="width: 100%">Burger</p> 
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Chocolate.jpg" onclick = "myFunctionK()" alt "Submit" style="width: 100%">Chocolate</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Corn.jpg" onclick = "myFunctionL()" alt "Submit" style="width: 100%">Corn</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Eggs.jpg" onclick = "myFunctionM()" alt "Submit" style="width: 100%">Eggs</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Broccoli.jpg" onclick = "myFunctionN()" alt "Submit" style="width: 100%">Broccoli</p> <p style="clear: both;">
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Ahi.jpg" onclick = "tai()" alt "Submit" style="width: 100%">Ahi</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Blueberries.jpg" onclick = "tbs()" alt "Submit" style ="width: 100%">Blueberries</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Things/FoodandDrinks/Burger.jpg" onclick = "tbr()" alt "Submit" style ="width: 100%">Burger</p> 
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Chocolate.jpg" onclick = "tchoc()" alt "Submit" style="width: 100%">Chocolate</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Corn.jpg" onclick = "tcn()" alt "Submit" style="width: 100%">Corn</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Eggs.jpg" onclick = "tes()" alt "Submit" style="width: 100%">Eggs</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Broccoli.jpg" onclick = "tbi()" alt "Submit" style="width: 100%">Broccoli</p> <p style="clear: both;">
 			
 			<!Row 3>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 12%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src ="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Water.jpg" onclick = "myFunctionO()" alt="Submit" style="width: 100%">Water</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Bread.jpg" onclick = "myFunctionP()" alt="Submit" style="width: 100%">Bread</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Cabbage.jpg" onclick = "myFunctionQ()" alt="Submit" style="width: 100%">Cabbage</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 12%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Carrots.jpg" onclick = "myFunctionR()" alt="Submit" style="width: 100%">Carrots</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Cheese.png" onclick = "myFunctionS()" alt="Submit" style="width: 100%">Cheese</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 12%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/ChickenWings.jpg" onclick = "myFunctionT()" alt="Submit" style="width: 100%">Chicken wings</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 12%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Cake.jpg" onclick = "myFunctionU()" alt="Submit" style="width: 100%">Cake</p> <p style="clear: both;">
+			<p style="float: left; font-size: 12pt; text-align: center; width: 12%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src ="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Water.jpg" onclick = "twr()" alt="Submit" style="width: 100%">Water</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Bread.jpg" onclick = "tbd()" alt="Submit" style="width: 100%">Bread</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Cabbage.jpg" onclick = "tcabb()" alt="Submit" style="width: 100%">Cabbage</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 12%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Carrots.jpg" onclick = "tcs()" alt="Submit" style="width: 100%">Carrots</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 13%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Cheese.png" onclick = "tce()" alt="Submit" style="width: 100%">Cheese</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 12%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/ChickenWings.jpg" onclick = "tcw()" alt="Submit" style="width: 100%">Chicken wings</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 12%; margin-right: 1%; margin-bottom: 0.5em;"><input type="image" img src="file://E|/ProgramImageDirectory/Things/FoodandDrinks/Cake.jpg" onclick = "tcake()" alt="Submit" style="width: 100%">Cake</p> <p style="clear: both;">
   
 		<br>
 		<br>
@@ -84,87 +84,87 @@
   
 	<script>
 	/* These are the functions that get called when the user clicks the buttons on the program page*/
-		function myFunctionA() {
+		function tdy() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Dairy");
 		}
-		function myFunctionB() {
+		function tgs() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Grains");
 		}
-		function myFunctionC() {
+		function tfs() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300"); 
    			myWindow.document.write("<p>Fruits");
 		}
-		function myFunctionD() {
+		function tvs() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Vegetables");
 		}
-		function myFunctionE() {
+		function tmt() {
    			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Meat");
 		}
-		function myFunctionF() {
+		function tmk() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Milk");
 		}
-		function myFunctionG() {
+		function tae() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Apple");
 		}
-		function myFunctionH() {
+		function tai() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Ahi");
 		}
-		function myFunctionI() {
+		function tbs() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
    			myWindow.document.write("<p>Blueberries");
 		}
-		function myFunctionJ() {
+		function tbr() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Burger");
 		}
-		function myFunctionK() {
+		function tchoc() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300"); 
     			myWindow.document.write("<p>Chocolate");
 		}
-		function myFunctionL() {
+		function tcn() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Corn");
 		}
-		function myFunctionM() {
+		function tes() {
    			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Eggs");
 		}
-		function myFunctionN() {
+		function tbi() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Broccoli");
 		}
-		function myFunctionO() {
+		function twr() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
    			myWindow.document.write("<p>Water");
 		}
-		function myFunctionP() {
+		function tbd() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Bread");
 		}
-		function myFunctionQ() {
+		function tcabb() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Cabbage");
 		}
-		function myFunctionR() {
+		function tcs() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Carrots");
 		}
-		function myFunctionS() {
+		function tce() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Cheese");
 		}
-		function myFunctionT() {
+		function tcw() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
    			myWindow.document.write("<p>Chicken wings");
 		}
-		function myFunctionU() {
+		function tcake() {
     			var myWindow = window.open("", "MsgWindow3", "width=300,height=300");
     			myWindow.document.write("<p>Cake");
 		}
