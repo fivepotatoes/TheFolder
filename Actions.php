b85930b091cb3120478cbb0d6dec8d5a279d4810
fivepotatoes <35906518+fivepotatoes@users.noreply.github.com>
2018-02-09 00:48:46 -1000

    Update Actions.php

--- Actions.php
+++ Actions.php
@@ -7,115 +7,106 @@
 		
 			<!Row 1>
 			
-			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Give.jpg" onclick = "myFunctionQ()" alt="Submit" style="width: 100%">Give</p> 
-			<a href="file://E|/alt/EatSelect.php"><p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Male/Eat.jpg" onclick = "myFunctionW()" alt="Submit" style ="width: 100%">Eat</p></a> 	
-			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src="file://E|/ProgramImageDirectory/Actions/Listen.jpg" onclick = "myFunctionE()" alt="Submit" style="width: 100%">Listen</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src="file://E|/ProgramImageDirectory/Actions/Male/Sleep.jpeg" onclick = "myFunctionR()" alt="Submit" style="width: 100%">Sleep</p> 
-			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src="file://E|/ProgramImageDirectory/Actions/Talk.jpg" onclick = "myFunctionT()" alt="Submit" style="width: 100%">Talk</p><p style="clear: both;"> 
+			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Give.jpg" onclick = "tgive()" alt="Submit" style="width: 100%">Give</p> 
+			<a href="file://E|/alt/EatSelect.php"><p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Male/Eat.jpg" onclick = "teat()" alt="Submit" style ="width: 100%">Eat</p></a> 	
+			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src="file://E|/ProgramImageDirectory/Actions/Listen.jpg" onclick = "tlisten()" alt="Submit" style="width: 100%">Listen</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src="file://E|/ProgramImageDirectory/Actions/Male/Sleep.jpeg" onclick = "tsleep()" alt="Submit" style="width: 100%">Sleep</p> 
+			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src="file://E|/ProgramImageDirectory/Actions/Talk.jpg" onclick = "ttalk()" alt="Submit" style="width: 100%">Talk</p><p style="clear: both;"> 
 			
 			<!Row 2>
 			
-			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Female/Sit.jpg" onclick = "myFunctionY()" alt="Submit" style="width: 100%">Sit</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Female/Think.jpg" onclick = "myFunctionU()" alt="Submit" style="width: 100%">Think</p>
-			<a href="file://E|/alt/WalkToSelect.php"><p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Male/Walk.jpg" onclick = "myFunctionI()" alt="Submit" style="width: 100%">Walk</p></a>
-			<a href="file://E|/alt/DrinkSelect.php"><p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Male/Drink.jpg" onclick = "myFunctionO()" alt="Submit" style="width: 100%">Drink</p></a>
-			<a href="file://E|/alt/BuySelect.php"><p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Male/Buy.jpg" onclick = "myFunctionP()" alt="Submit" style="width: 100%">Buy</p><p style="clear: both;"></a> 
+			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Female/Sit.jpg" onclick = "tsit()" alt="Submit" style="width: 100%">Sit</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Female/Think.jpg" onclick = "tthink()" alt="Submit" style="width: 100%">Think</p>
+			<a href="file://E|/alt/WalkToSelect.php"><p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Male/Walk.jpg" onclick = "twalk()" alt="Submit" style="width: 100%">Walk</p></a>
+			<a href="file://E|/alt/DrinkSelect.php"><p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Male/Drink.jpg" onclick = "tdrink()" alt="Submit" style="width: 100%">Drink</p></a>
+			<a href="file://E|/alt/BuySelect.php"><p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Male/Buy.jpg" onclick = "tbuy()" alt="Submit" style="width: 100%">Buy</p><p style="clear: both;"></a> 
 			
 			<!Row 3>
 			
-			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Close.png" onclick = "myFunctionA()" alt="Submit" style="width: 100%">Close</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Open.jpg" onclick = "myFunctionS()" alt="Submit" style="width: 100%">Open</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Take.jpg" onclick = "myFunctionD()" alt="Submit" style="width: 100%">Take</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/PhoneCall.jpg" onclick = "myFunctionF()" alt="Submit" style="width: 100%">Call</p>
-			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Stop.png" onclick = "myFunctionG()" alt="Submit" style="width: 100%">Stop</p><p style="clear: both;">
+			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Close.png" onclick = "tclose()" alt="Submit" style="width: 100%">Close</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Open.jpg" onclick = "topen()" alt="Submit" style="width: 100%">Open</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Take.jpg" onclick = "ttake()" alt="Submit" style="width: 100%">Take</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/PhoneCall.jpg" onclick = "tcall()" alt="Submit" style="width: 100%">Call</p>
+			<p style="float: left; font-size: 12pt; text-align: center; width: 18.5%; margin-right: 1%; margin-bottom: 0.1em;"><input type="image" img src = "file://E|/ProgramImageDirectory/Actions/Stop.png" onclick = "tstop()" alt="Submit" style="width: 100%">Stop</p><p style="clear: both;">
 			
 		<div align=left></left>
 			
 		<!	<img src="imageLocation" style="float: left; width: 30%; margin-right: 1%; margin-bottom: 0.1em;">
 		
-		function makecoffee($type = "cappuccino")
-{
-    return "Making a cup of $type.\n";
-}
-echo makecoffee();
-echo makecoffee(null);
-echo makecoffee("espresso");
-		
 	<script>
 	<!/* These are the functions that get called when the user clicks the buttons on the program page*/>
-		function Iam($type = "giving this") {
+		function tgive() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
-    			myWindow.document.write("<p>I am $type.\n");
+    			myWindow.document.write("<p>I am giving this.\n");
 		}
 	<!/* Function pops up a window with the translation dialogue, same below.*/>
-		echo Iam("listening");
 		
-		function myFunctionW() {
+		function teat() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
     			myWindow.document.write("<p>I will eat. <br><br>OR <br><br> I am eating.");
 		}
 
-		function myFunctionE() {
+		function tlisten() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300"); 
     			myWindow.document.write("<p>I am listening.");
 		}
 
-		function myFunctionR() {
+		function tsleep() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
     			myWindow.document.write("<p>I want to sleep. <br><br>OR <br><br>Sleep.");
 		}
 
-		function myFunctionT() {
+		function ttalk() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
     			myWindow.document.write("<p>I will talk. <br><br>OR <br><br>Let's talk.");
 		}
 
-		function myFunctionY() {
+		function tsit() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
     			myWindow.document.write("<p>I will sit.");
 		}
 
-		function myFunctionU() {
+		function tthink() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
     			myWindow.document.write("<p>I am thinking. <br><br>OR <br><br>I will think about it.");
 		}
 
-		function myFunctionI() {
+		function twalk() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
     			myWindow.document.write("<p>I will walk. <br><br>OR <br><br>I am walking.");
 		}
 
-		function myFunctionO() {
+		function tdrink() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
     			myWindow.document.write("<p>I will drink. <br><br>OR <br><br>I am drinking.");
 		}
 
-		function myFunctionP() {
+		function tbuy() {
    			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
     			myWindow.document.write("<p>I want to buy. <br><br>OR <br><br>I will buy.");
 		}
 
-		function myFunctionA() {
+		function tclose() {
    			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
    			myWindow.document.write("<p>Close it. <br><br>OR <br><br>I will close it.");
 		}
 
-		function myFunctionS() {
+		function topen() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
     			myWindow.document.write("<p>Open it. <br><br>OR <br><br>I will open it.");
 		}
 
-		function myFunctionD() {
+		function ttake() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
     			myWindow.document.write("<p>I am taking this.");
 		}
 
-		function myFunctionF() {
+		function tcall() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
     			myWindow.document.write("<p>I am calling <br><br>OR <br><br> I will call.");
 		}
 
-		function myFunctionG() {
+		function tstop() {
     			var myWindow = window.open("", "MsgWindow1", "width=300,height=300");
     			myWindow.document.write("<p>Stop (or I will stop.)");
 		}
